171616de665c4aebb9191ef3dafc13cfbe7c2cdf
Jor Bashllari <altnate2000@gmail.com>
2018-12-18 20:08:36 +0100

    gets proper line hopefully

---
+++ get_next_line.c
@@ -0,0 +1,81 @@
+/*
+** EPITECH PROJECT, 2018
+** CPE_getnextline_2018
+** File description:
+** GNL implementation
+*/
+
+#include <stdio.h>
+#include "get_next_line.h"
+
+static int my_strlen(char const *s)
+{
+    int i = 0;
+
+    while (s && s[i])
+        i++;
+    return i;
+}
+
+char *my_strncpy(char *dest, char const *src, int n)
+{
+    int i = 0;
+
+    if (!src || n < 1)
+        return 0;
+    for (; src[i] && i < n; i++)
+        dest[i] = src[i];
+    if (i > n)
+        dest[i] = '\0';
+    return dest;
+}
+
+static char *append(char *lnbuf, int n, gnl_t *f)
+{
+    int oldlen = my_strlen(lnbuf);
+    char *newlen = malloc((oldlen + n + 1) * sizeof(*newlen));
+
+    my_strncpy(newlen, lnbuf, oldlen);
+    newlen[oldlen + n] = 0;
+    my_strncpy(newlen + oldlen, f->rbuf + f->ridx, n);
+    if (lnbuf)
+        free(lnbuf);
+    f->ridx = f->ridx + (n + 1);
+    return (newlen);
+}
+
+char *get_next_line(const int fd)
+{
+    static gnl_t f;
+    static int readret = 0;
+    int n = 0;
+    char *lnbuf = 0;
+
+    while (1) {
+        if (readret <= f.ridx) {
+            f.ridx = 0;
+            n = 0;
+            readret = read(fd, f.rbuf, READ_SIZE);
+            if (readret <= 0)
+                return (readret == 0) ? lnbuf : 0;
+        }
+        if (f.rbuf[f.ridx + n] == '\n')
+            return append(lnbuf, n, &f);
+        if (f.ridx + n == readret - 1)
+            lnbuf = append(lnbuf, n + 1, &f);
+        n++;
+    }
+}
+
+int main(void)
+{
+    int f = open("./get_next_line.c", O_RDONLY);
+    char *s = get_next_line(f);
+
+    while (s) {
+        write(1, s, my_strlen(s));
+        write(1, "\n", 1);
+        free(s);
+        s = get_next_line(f);
+    }
+}
---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2018
+** CPE_getnextline_2018
+** File description:
+** GNL header
+*/
+
+#ifndef GET_NEXT_LINE_H
+#    define GET_NEXT_LINE_H
+
+#    include <fcntl.h>
+#    include <stddef.h>
+#    include <stdlib.h>
+#    include <sys/types.h>
+#    include <unistd.h>
+
+#    ifndef READ_SIZE
+#        define READ_SIZE 100
+#    endif // READ_SIZE
+
+typedef struct fd_s {
+    int ridx;
+    int rbuflen;
+    char rbuf[READ_SIZE + 2];
+} gnl_t;
+
+void *gib(size_t);
+void *regib(char *, int);
+char *my_strmerge(char *, char const *);
+int is_in(char const, char const *);
+
+char *get_next_line(int);
+
+#endif // GET_NEXT_LINE_H
