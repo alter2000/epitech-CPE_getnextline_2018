99f6e2292ecf5389e609bb42292c86dd105932db
Jor Bashllari <altnate2000@gmail.com>
2018-12-23 20:43:29 +0100

    coding style header indentation

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,15 @@
 */
 
 #ifndef GET_NEXT_LINE_H
-#    define GET_NEXT_LINE_H
+    #define GET_NEXT_LINE_H
 
-#    include <fcntl.h>
-#    include <stdlib.h>
-#    include <unistd.h>
+    #include <fcntl.h>
+    #include <stdlib.h>
+    #include <unistd.h>
 
-#    ifndef READ_SIZE
-#        define READ_SIZE (100)
-#    endif // READ_SIZE
+    #ifndef READ_SIZE
+        #define READ_SIZE (100)
+    #endif // READ_SIZE
 
 typedef struct fd_s {
     int ridx;
